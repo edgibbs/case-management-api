04a9d63015dee00733997c8697044bfc896f155e
mikhailzemlyanoi <m.zemlyanoi@gmail.com>
2017-11-08 17:11:04 -0800

    - filled fixture for Child_Client entity - fixed google style format

--- src/test/java/gov/ca/cwds/cm/web/rest/ChildClientResourceTest.java
+++ src/test/java/gov/ca/cwds/cm/web/rest/ChildClientResourceTest.java
@@ -13,25 +13,21 @@ import javax.ws.rs.core.MediaType;
 import static gov.ca.cwds.cm.web.rest.utils.AssertResponseHelper.assertEqualsResponse;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 
-/**
- * @author CWDS TPT-3 Team
- */
+/** @author CWDS TPT-3 Team */
 public class ChildClientResourceTest extends BaseApiIntegrationTest {
 
-    public static final String CASE_NUMBER = "1";
+  public static final String CASE_NUMBER = "1";
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-    }
+  @BeforeClass
+  public static void beforeClass() throws Exception {}
 
-    @Test
-    public void testGetChildClientById() throws Exception {
-        WebTarget target = clientTestRule.target(Constants.API.CHILD_CLIENT + "/" + CASE_NUMBER);
-        Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON_TYPE);
-        ChildClientDTO childClientDTO = invocation.get(ChildClientDTO.class);
+  @Test
+  public void testGetChildClientById() throws Exception {
+    WebTarget target = clientTestRule.target(Constants.API.CHILD_CLIENT + "/" + CASE_NUMBER);
+    Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON_TYPE);
+    ChildClientDTO childClientDTO = invocation.get(ChildClientDTO.class);
 
-        String fixture = fixture("fixtures/child-client-by-id-response.json");
-        assertEqualsResponse(fixture, transformDTOtoJSON(childClientDTO));
-    }
-
+    String fixture = fixture("fixtures/child-client-by-id-response.json");
+    assertEqualsResponse(fixture, transformDTOtoJSON(childClientDTO));
+  }
 }
