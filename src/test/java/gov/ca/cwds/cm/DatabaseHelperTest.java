ae4b31e92cc156534e62ddc418eea43a0ec4136a
mikhailzemlyanoi <m.zemlyanoi@gmail.com>
2017-11-09 15:47:14 -0800

    - liquibase for child client entity finished - database helper - test for database helper

---
+++ src/test/java/gov/ca/cwds/cm/DatabaseHelperTest.java
@@ -0,0 +1,40 @@
+package gov.ca.cwds.cm;
+
+import gov.ca.cwds.cm.web.BaseApiIntegrationTest;
+import liquibase.exception.LiquibaseException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** @author CWDS TPT-2 Team */
+public class DatabaseHelperTest extends BaseApiIntegrationTest {
+
+  DatabaseHelper helper;
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    setUpDb();
+  }
+
+  @Before
+  public void before() {
+    helper = getDatabaseHelper();
+  }
+
+  @Test
+  public void runScript() throws Exception {
+    Map<String, Object> params = new HashMap<>();
+    helper.runScript("liquibase/test_database_master.xml", params, null);
+
+    try {
+      helper.runScript("liquibase/test_database_master.xml", params, "");
+      Assert.fail();
+    } catch (LiquibaseException e) {
+      // expected
+    }
+  }
+}
