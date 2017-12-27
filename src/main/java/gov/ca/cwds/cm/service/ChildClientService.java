b7012dc122fea6369ad7b209e4dce0f381a36c5d
Mykhailo <m.zemlyanoi@gmail.com>
2017-12-27 14:04:25 -0800

    - fix sonar

--- src/main/java/gov/ca/cwds/cm/service/ChildClientService.java
+++ src/main/java/gov/ca/cwds/cm/service/ChildClientService.java
@@ -1,19 +1,13 @@
 package gov.ca.cwds.cm.service;
 
 import com.google.inject.Inject;
-import gov.ca.cwds.cm.service.builder.ClientEntityAwareDTOBuilder;
 import gov.ca.cwds.cm.service.dto.ChildClientDTO;
 import gov.ca.cwds.cm.service.mapper.ChildClientMapper;
 import gov.ca.cwds.cm.web.rest.parameter.ClientParameterObject;
-import gov.ca.cwds.cms.data.access.dto.ClientEntityAwareDTO;
 import gov.ca.cwds.cms.data.access.service.ClientCoreService;
-import gov.ca.cwds.cms.data.access.service.DataAccessServicesException;
 import gov.ca.cwds.data.legacy.cms.dao.ChildClientDao;
 import gov.ca.cwds.data.legacy.cms.entity.ChildClient;
 
-import gov.ca.cwds.data.legacy.cms.entity.Client;
-import gov.ca.cwds.rest.api.Request;
-import gov.ca.cwds.rest.api.Response;
 import java.io.Serializable;
 
 /** @author CWDS TPT-3 Team */
@@ -40,20 +34,4 @@ public class ChildClientService extends CrudServiceAdapter {
     return childClientMapper.toChildClientDTO(childClient);
   }
 
-  @Override
-  public Response update(Serializable serializable, Request request) {
-    ChildClient entity = childClientMapper.fromChildClientDTO((ChildClientDTO) request);
-    ChildClient childClient = (ChildClient)updateClient(entity);
-    return childClientMapper.toChildClientDTO(childClient);
-  }
-
-  private Client updateClient(Client client) {
-    ClientEntityAwareDTOBuilder builder = new ClientEntityAwareDTOBuilder(client);
-    ClientEntityAwareDTO clientEntityAwareDTO = builder.build();
-    try {
-      return clientCoreService.update(clientEntityAwareDTO);
-    } catch (DataAccessServicesException e) {
-      throw new RuntimeException(e);
-    }
-  }
 }
