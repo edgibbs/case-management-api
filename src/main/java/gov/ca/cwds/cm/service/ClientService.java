be8afcf669f03f5b0031b6c00907c3dc884910f0
dmytro.palczewski <dmytro.palczewski@engagepoint.com>
2017-12-27 10:54:42 -0800

    TPT3-92: clientCoreService invocation is integrated in ChildClient update

--- src/main/java/gov/ca/cwds/cm/service/ClientService.java
+++ src/main/java/gov/ca/cwds/cm/service/ClientService.java
@@ -1,13 +1,10 @@
 package gov.ca.cwds.cm.service;
 
 import com.google.inject.Inject;
-import gov.ca.cwds.cm.service.builder.ClientEntityAwareDTOBuilder;
 import gov.ca.cwds.cm.service.dto.ClientDTO;
 import gov.ca.cwds.cm.service.mapper.ClientMapper;
 import gov.ca.cwds.cm.web.rest.parameter.ClientParameterObject;
-import gov.ca.cwds.cms.data.access.dto.ClientEntityAwareDTO;
 import gov.ca.cwds.cms.data.access.service.ClientCoreService;
-import gov.ca.cwds.cms.data.access.service.DataAccessServicesException;
 import gov.ca.cwds.data.legacy.cms.dao.ClientDao;
 import gov.ca.cwds.data.legacy.cms.entity.Client;
 import java.io.Serializable;
@@ -31,17 +28,5 @@ public class ClientService extends CrudServiceAdapter {
   public ClientDTO find(Serializable serializable) {
     Client client = clientDao.find(((ClientParameterObject) serializable).getClientId());
     return clientMapper.toClientDTO(client);
-  }
-
-  public void update(Serializable serializable) {
-    clientDao.update(clientMapper.toClient((ClientDTO) serializable));
-  }
-
-  //use this method at update() implementation
-  private Client updateClient(Client client) throws DataAccessServicesException {
-    ClientEntityAwareDTOBuilder builder =
-        new ClientEntityAwareDTOBuilder(client);
-    ClientEntityAwareDTO clientEntityAwareDTO = builder.build();
-    return clientCoreService.update(clientEntityAwareDTO);
   }
 }
