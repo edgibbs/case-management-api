fad1420797c13ae6f48a5fd6888a88bcdb1d77c1
dmytro.palczewski <dmytro.palczewski@engagepoint.com>
2017-12-28 16:47:34 -0800

    TPT3-50: update() echo method is added

--- src/main/java/gov/ca/cwds/cm/web/rest/CaseResource.java
+++ src/main/java/gov/ca/cwds/cm/web/rest/CaseResource.java
@@ -16,15 +16,14 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-/**
- * @author CWDS TPT-3 Team
- */
+/** @author CWDS TPT-3 Team */
 @Api(tags = CASES, value = CASES)
 @Path(value = CASES)
 @Produces(MediaType.APPLICATION_JSON)
@@ -41,21 +40,42 @@ public class CaseResource {
   @GET
   @Path("/{" + ID + "}")
   @ApiResponses(
-      value = {
-          @ApiResponse(code = 401, message = "Not Authorized"),
-          @ApiResponse(code = 404, message = "Not found"),
-          @ApiResponse(code = 406, message = "Accept Header not supported")
-      }
+    value = {
+      @ApiResponse(code = 401, message = "Not Authorized"),
+      @ApiResponse(code = 404, message = "Not found"),
+      @ApiResponse(code = 406, message = "Accept Header not supported")
+    }
   )
   @UnitOfWork(CMS)
   @Timed
   @ApiOperation(value = "Find Case by case ID", response = CaseDTO.class)
   public Response get(
-      @PathParam(ID)
-      @ApiParam(required = true, value = "The unique case ID", example = "AadfKnG07n")
-      final String id) {
+    @PathParam(ID)
+    @ApiParam(required = true, value = "The unique case ID", example = "AadfKnG07n")
+    final String id) {
     CaseDTO caseDTO = caseService.find(id);
     return ResponseUtil.responseOrNotFound(caseDTO);
   }
 
+  @PUT
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Timed
+  @ApiResponses(
+    value = {
+      @ApiResponse(code = 400, message = "Bad request"),
+      @ApiResponse(code = 404, message = "Not Found"),
+      @ApiResponse(code = 401, message = "Not Authorized"),
+      @ApiResponse(code = 406, message = "Accept Header not supported")
+    }
+  )
+  @ApiOperation(value = "Update case", response = CaseDTO.class)
+  public Response update(
+      @PathParam("id")
+      @ApiParam(required = true, value = "The unique case ID", example = "AadfKnG07n")
+      final String id,
+      @ApiParam(name = "caseData", value = "The Case data")
+      CaseDTO caseDTO) {
+    return ResponseUtil.responseOrNotFound(caseDTO);
+  }
 }
